a3cec15d9c0bd1a935d28e3c6638dd7104b456c5
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-01-21 11:30:08 +0100

    created a package with paster and uploaded it to pypi

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.2'
+
+setup(name='brynjar',
+      version=version,
+      description="First package for python course",
+      long_description=""" """,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='python scilifelab',
+      author='Brynjar Smari Bjarnason',
+      author_email='binni@binnisb.com',
+      url='https://github.com/binnisb',
+      license='GPLv3',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

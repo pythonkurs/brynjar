4757e6703859dca94b6e0725b5a0ed6b53336888
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-01-23 17:13:01 +0100

    parce and return fractions of escalators that are in repair

--- brynjar/__init__.py
+++ brynjar/__init__.py
@@ -1,1 +1,31 @@
-#
+import requests
+import untangle
+
+
+
+def _get_structure(url):
+    req = requests.get(url)
+    struct = untangle.parse(req.text)
+    return struct
+
+def _get_escalators_counts(struct):
+    esc = []
+    esc_repair = []
+    for outage in struct.NYCOutages.outage:
+        if outage.equipmenttype=="ES":
+            esc.append(outage)
+            if outage.reason == "REPAIR":
+                esc_repair.append(outage)
+
+    
+    return (len(esc_repair),len(esc))
+
+def _get_esc_repair_frac(url):
+    struct = _get_structure(url)
+    (esc_repair,esc_tot) = _get_escalators_counts(struct)
+    return float(esc_repair)/float(esc_tot)
+    
+
+def get_esc_repair_frac_NYCT():
+    url = "http://www.grandcentral.org/developers/data/nyct/nyct_ene.xml"
+    return _get_esc_repair_frac(url)
---
+++ scripts/getting_data.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import sys
+sys.path.append("../")
+import brynjar as b
+frac = b.get_esc_repair_frac_NYCT()
+:q
+:wq
+print frac

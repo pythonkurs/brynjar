c881f3431648cc02b61277df5079ae0f5b39bce9
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-02-06 19:31:13 +0100

    Done find most common day and hour

--- brynjar/session4.py
+++ brynjar/session4.py
@@ -1,27 +1,41 @@
 # -*- coding: utf-8 -*-
-import requests
-from dateutil import parser
-with open("../secret") as secret:
-    password = secret.read().strip()
-AUTH = ("binnisb",password)
-COMMITS_URL =  "https://api.github.com/repos/pythonkurs/{0}/commits"
-REPOS_URL = "https://api.github.com/orgs/pythonkurs/repos"
-
+import operator
+def get_commits_for_pythonkurs(limit=100000):
+    import requests
+    from dateutil import parser
+    from pandas import DataFrame,Series
+    with open("/home/binni/MasterProject/scilifelabpython/Brynjar/secret") as secret:
+        password = secret.read().strip()
+    AUTH = ("binnisb",password)
+    COMMITS_URL =  "https://api.github.com/repos/pythonkurs/{0}/commits"
+    REPOS_URL = "https://api.github.com/orgs/pythonkurs/repos"
     
-users = requests.get(REPOS_URL, auth=AUTH)
-users_data = users.json()
-print len(users_data)
-users_name = [user["name"] for user in users_data]
+    users = requests.get(REPOS_URL, auth=AUTH)
+    users_data = users.json()
+    users_name = [user["name"] for user in users_data]
+    commit_histories = {}
+    for user in users_name[:limit]:
+        commit_response = requests.get(COMMITS_URL.format(user),auth=AUTH)
+        commits = commit_response.json()
+        ser = []
+        ind = []
+        for d in commits:
+            ser.append(d["commit"]["message"])
+            ind.append(parser.parse(d["commit"]["author"]["date"]))
+        commit_histories[user] = Series(data=ser,index=ind,name=user,dtype=str)
+    df = DataFrame(commit_histories)
+    return df
 
-commit_histories = []
-for user in users_name:
-    print user
-    commit_response = requests.get(COMMITS_URL.format(user),auth=AUTH)
-    
-    commits = commit_response.json()
-    print commits
-    for d in commits:
-        c = [d["commit"]["message"], parser.parse(d["commit"]["author"]["date"])]
-        commit_histories.append(c)
-        print c
-print len(commit_histories)
+def get_most_common_day_and_hour(df):
+    day = _group_by_key(lambda x: x.strftime("%A"),df)
+    hour = _group_by_key(lambda x: x.hour,df)
+    return (day,hour)
+
+def _group_by_key(lamda_func,df):
+    groupped = df.groupby(lamda_func)
+    return max(groupped.groups.iterkeys(),key=(lambda k: len(groupped.groups[k])))
+
+if __name__=="__main__":
+    df = get_commits_for_pythonkurs()
+    (day,hour) = get_most_common_day_and_hour(df)
+    print day,hour

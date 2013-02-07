88b1a2aacac964ee1d674b519d25cb3b024ec165
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-02-07 17:20:54 +0100

    Small change due to comment on github

--- scripts/check_repo.py
+++ scripts/check_repo.py
@@ -4,7 +4,7 @@ import os
 from brynjar.session3 import DirChanger, CourseRepo
 (basepath,repo)=os.path.split(sys.argv[1])
 
-with DirChanger(basepath) as dc:
+with DirChanger(basepath):
     a = CourseRepo(repo)
     if a.check():
         print "PASS"
